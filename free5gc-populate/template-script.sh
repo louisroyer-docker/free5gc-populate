a3fa43fbe12cf1db5808b67050fbf1f5cc1997f1
Louis Royer <infos.louis.royer@gmail.com>
2023-08-24 11:06:41 +0200

    Fix newlines issues

--- free5gc-populate/template-script.sh
+++ free5gc-populate/template-script.sh
@@ -13,16 +13,46 @@ if [ -z "$IMSI" ]; then
 	exit 1
 fi
 
-sed \
-	-e "s/%MONGO_HOST/${MONGO_HOST}/g" \
-	-e "s/%MONGO_PORT/${MONGO_PORT:-27017}/g" \
-	-e "s/%MONGO_NAME/${MONGO_NAME:-free5gc}/g" \
-	-e "s/%MCC/${MCC:-001}/g" \
-	-e "s/%MNC/${MNC:-01}/g" \
-	-e "s/%KEY/${KEY:-8baf473f2f8fd09487cccbd7097c6862}/g" \
-	-e "s/%OP/${OP:-8e27b6af0e692e750f32667a3b14605d}/g" \
-	-e "s/%SQN/${SQN:-16f3b3f70fc2}/g" \
-	-e "s/%AMF/${AMF:-8000}/g" \
-	-e "s/%SLICES/${SLICES}/g" \
-	-e "s/%IMSI/${IMSI}/g" \
-"${CONFIG_TEMPLATE}" > "${CONFIG_FILE}"
+IFS=$'\n'
+SLICES_SUB=""
+for SLICE in ${SLICES}; do
+	if [ -n "${SLICE}" ]; then
+		SLICES_SUB="${SLICES_SUB}\n  ${SLICE}"
+	fi
+done
+
+IMSI_SUB=""
+for I_IMSI in ${IMSI}; do
+	if [ -n "${I_IMSI}" ]; then
+		IMSI_SUB="${IMSI_SUB}\n  ${I_IMSI}"
+	fi
+done
+echo $IMSI_SUB
+
+awk \
+	-v MONGO_HOST="${MONGO_HOST}" \
+	-v MONGO_PORT="${MONGO_PORT:-27017}" \
+	-v MONGO_NAME="${MONGO_NAME:-free5gc}" \
+	-v MCC="${MCC:-001}" \
+	-v MNC="${MNC:-01}" \
+	-v KEY="${KEY:-8baf473f2f8fd09487cccbd7097c6862}" \
+	-v OP="${OP:-8e27b6af0e692e750f32667a3b14605d}" \
+	-v SQN="${SQN:-16f3b3f70fc2}" \
+	-v AMF="${AMF:-8000}" \
+	-v SLICES="${SLICES_SUB}" \
+	-v IMSI="${IMSI_SUB}" \
+	'{
+		sub(/%MONGO_HOST/, MONGO_HOST);
+		sub(/%MONGO_PORT/, MONGO_PORT);
+		sub(/%MONGO_NAME/, MONGO_NAME);
+		sub(/%MCC/, MCC);
+		sub(/%MNC/, MNC);
+		sub(/%KEY/, KEY);
+		sub(/%OP/, OP);
+		sub(/%SQN/, SQN);
+		sub(/%AMF/, AMF);
+		sub(/%SLICES/, SLICES);
+		sub(/%IMSI/, IMSI);
+		print;
+	}' \
+	"${CONFIG_TEMPLATE}" > "${CONFIG_FILE}"
